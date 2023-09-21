5ec786584cd961be647b7b8d0cfc35f34fb0302c
deepakthecoder1994 <deepakyad18@gmail.com>
2023-09-21 19:20:48 +0530

    Done Currency Converter App done

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# currency_converter
-A Currency Converter Application for Window Applications build using Electron.js
+# Welcome to Currency Converter 
+
+## A Currency Converter Application for Window Applications build using Electron.js
+
+### This the starting page for currency converter 
+
+![Alt text](image.png)
+
+
+### You can convert over 100+ countries currency into different currencies 
+
+![Alt text](image-1.png)
+
+### For getting the conversion of desired country currency you just need to select from the select tag and then 
+### to which country you want to convert. Also You can enter the amount of money you want to convert 
+** For example here I am converting 120 USd dollars to Bharat(before known as India) rupees.
+
+![Alt text](image-2.png)
+
+** You can see the output **
+
+### If you want to switch between the same country's in the options you can just click the switch icon between 
+### them and can switch between them in an second and convert accordingly. 
+**See the example below for more details. **
+
+![Alt text](image-3.png)
+
+
+## For Running it locally into Your system `#0969DA` 
+## Follow the Below Steps >
+```
+git clone https://github.com/deepakthecoder1982/currency_converter.git
+
+<!-- Make sure that Node is installed in your system for running and working on this application -->
+<!-- Then For Running the application follow below steps -->
+
+cd currency_converter
+
+npm install 
+
+npx electron .
+
+<!-- Congratulations You have succesfully run the application in your system!! -->
+```
+
+## If you have anything to share with me or wanted to provide me any feedback, please feel free to connect with me over my linkdin account or email address. Also If you stuck somewhere don't hesitate to contact me at my email address below :- 
+`
+Linkedin Account :- https://www.linkedin.com/in/deepak-archana-mahesh-yadav/
+
+Email Address :- deepakyad18@gmail.com 
+`
+
+### Thank you for reading till this !! Have a great future ahead 
+
+# Keep Coding ! Keep Exploring !
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let country_choice = document.querySelectorAll(".converter select");
 let country_image = document.querySelectorAll(".converter .children img");
 // require("dotenv").config();
+const APIkey = "5a485c84292e9954fa87da67";
 
 for (let i = 0; i < country_choice.length; i++) {
   for (currency_code in country_list) {
@@ -9,65 +10,82 @@ for (let i = 0; i < country_choice.length; i++) {
   }
 }
 
-document.querySelector("#fromCountry").addEventListener("change", (e)=>{
-    const {value} = e.target;
-    let image = document.querySelector(".converter .from_converter img");
-    image.setAttribute("src", `https://www.countryflagicons.com/FLAT/64/${value}.png`||`https://www.countryflagicons.com/FLAT/64/US.png`);
-
-})
-document.querySelector("#toCountry").addEventListener("change", (e)=>{
-    const {value} = e.target;
-    let image = document.querySelector(".converter .to_converter img");
-    image.setAttribute("src", `https://www.countryflagicons.com/FLAT/64/${value}.png`);
-
-})
+document.querySelector("#fromCountry").addEventListener("change", (e) => {
+  const { value } = e.target;
+  let image = document.querySelector(".converter .from_converter img");
+  image.setAttribute(
+    "src",
+    `https://www.countryflagicons.com/FLAT/64/${value}.png` ||
+      `https://www.countryflagicons.com/FLAT/64/US.png`
+  );
+});
+document.querySelector("#toCountry").addEventListener("change", (e) => {
+  const { value } = e.target;
+  let image = document.querySelector(".converter .to_converter img");
+  image.setAttribute(
+    "src",
+    `https://www.countryflagicons.com/FLAT/64/${value}.png`
+  );
+});
 
 let Converter_btn = document.getElementById("convert-btn");
-Converter_btn.addEventListener("click",async(e)=>{
+Converter_btn.addEventListener("click", async (e) => {
   let fromConverter = document.getElementById("fromCountry").value;
   e.preventDefault();
-  for(let country_code in country_list){
-      if(country_list[country_code] == fromConverter){
-          fromConverter = country_code;
-          break;
-      }
+  for (let country_code in country_list) {
+    if (country_list[country_code] == fromConverter) {
+      fromConverter = country_code;
+      break;
+    }
   }
-    try {
-      let data = await fetch(`https://v6.exchangerate-api.com/v6/${APIkey}/latest/${fromConverter}`).then(res=>res.json())
-      console.log(data)
-      let toConverter = document.getElementById("toCountry").value;
-      let amount = document.getElementById("amount").value;
+  try {
+    let data = await fetch(
+      `https://v6.exchangerate-api.com/v6/${APIkey}/latest/${fromConverter}`
+    ).then((res) => res.json());
+    console.log(data);
+    let toConverter = document.getElementById("toCountry").value;
+    let amount = document.getElementById("amount").value;
 
-      for(let country_code in country_list){
-        if(country_list[country_code] == toConverter){
-            toConverter = country_code;
-            break;
-        }
+    for (let country_code in country_list) {
+      if (country_list[country_code] == toConverter) {
+        toConverter = country_code;
+        break;
+      }
     }
-    let final_converson =amount * data.conversion_rates[toConverter]
+    let final_converson = amount * data.conversion_rates[toConverter];
 
-      document.querySelector(".conversion_rate").innerHTML = `<p>${amount} ${fromConverter} = ${final_converson.toFixed(2)} ${toConverter}</p>`
-      
-    } catch (error) {
-      console.log(error)
-    }
-})
+    document.querySelector(
+      ".conversion_rate"
+    ).innerHTML = `<p>${amount} ${fromConverter} = ${final_converson.toFixed(
+      2
+    )} ${toConverter}</p>`;
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+document
+  .querySelector(".switchingArrow")
+  .addEventListener("click", async () => {
+    let fromConverter = document.querySelector(".from_converter .children");
+    let toConverter = document.querySelector(".to_converter .children");
+    let amount = document.getElementById("amount").value;
+    let toHtml = toConverter.innerHTML;
+    let FromHtml = fromConverter.innerHTML;
+    let from_Converter = document.getElementById("fromCountry").value;
+    let to_Converter = document.getElementById("toCountry").value;
 
-document.querySelector(".switchingArrow").addEventListener("click",async()=>{
-  let fromConverter = document.querySelector(".from_converter .children");
-  let toConverter = document.querySelector(".to_converter .children");
-  let amount = document.getElementById("amount").value;
-  let toHtml = toConverter.innerHTML;
-  let FromHtml = fromConverter.innerHTML;
-  let from_Converter = document.getElementById("fromCountry").value;
-  let to_Converter = document.getElementById("toCountry").value;
-  
-  fromConverter.innerHTML = toHtml;
-  toConverter.innerHTML = FromHtml;
+    fromConverter.innerHTML = toHtml;
+    toConverter.innerHTML = FromHtml;
 
-  let data = await fetch(`https://v6.exchangerate-api.com/v6/${APIkey}/latest/${fromConverter.value}`).then(res=>res.json())
-  console.log(data)
+    let data = await fetch(
+      `https://v6.exchangerate-api.com/v6/${APIkey}/latest/${fromConverter.value}`
+    ).then((res) => res.json());
+    console.log(data);
 
-  document.querySelector(".conversion_rate").innerHTML = `<p>${amount} ${from_Converter} = ${amount * data.conversion_rates[to_Converter]} ${to_Converter}</p>`
-})
+    document.querySelector(
+      ".conversion_rate"
+    ).innerHTML = `<p>${amount} ${from_Converter} = ${
+      amount * data.conversion_rates[to_Converter]
+    } ${to_Converter}</p>`;
+  });
